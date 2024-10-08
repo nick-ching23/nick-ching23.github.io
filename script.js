67dbeee0100e9386d30fbfa0e64792ac5cd0dd41
Nicholas Ching <nicholasching@Nicholass-MacBook-Pro-7.local>
2024-10-08 17:57:30 -0400

    initial website

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("dynamic-button").addEventListener("click", function() {
+    const message = "Thank you for visiting my website!";
+    document.getElementById("dynamic-message").textContent = message;
+});
